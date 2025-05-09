d77ab5400aa34c302cb5bac9093c7e1e7fa36f14
marcofilip <filip.marco.studente@itispaleocapa.it>
2025-05-09 23:30:21 +0200

    migration addmaxuserstorooms

---
+++ Migrations/20250509211004_AddMaxUsersToRooms.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebateRoyale.Migrations
+{
+    public partial class AddMaxUsersToRooms : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxUsers",
+                table: "Rooms",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "MaxUsers",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "MaxUsers",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "MaxUsers",
+                value: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "MaxUsers",
+                value: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxUsers",
+                table: "Rooms");
+        }
+    }
+}
